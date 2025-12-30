5e84e011cb0c008ca6cfae58f96ec76f0eb12224
Ahsan462aggk <ahs462agk@gmail.com>
2025-12-31 02:11:35 +0500

    implement te changes

---
+++ fitness-ai-coach-frontend/next.config.mjs
@@ -0,0 +1,8 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  // ...other config options here
+};
+
+export default nextConfig;
--- fitness-ai-coach-frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-// next.config.ts
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  // ...other config options here
-};
-
-export default nextConfig;
